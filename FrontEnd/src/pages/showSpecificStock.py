8bc9fbf1f57117c13555d4699236c66bf6db2365
Gal M <gal@LAPTOP-DC7TDUNP.localdomain>
2023-01-18 15:53:33 +0200

    changed to multipage app

--- FrontEnd/src/pages/showSpecificStock.py
+++ FrontEnd/src/pages/showSpecificStock.py
@@ -8,11 +8,23 @@ import numpy as np
 import os
 import httpx
 
+#DATA_URL1 = "C:\Users\User\Desktop\BGU\gal\test"
+DATA_URL = 'test'
+#columns=["name", "email"]
 
 
 
 # Press the green button in the gutter to run the script.
 #if __name__ == '__main__':
 mainTitle=st.title ("""hello Gal""")
-st.sidebar.success("Select a demo above.")
+but=st.button("Submit")
+title=st.text_input("testing text","Enter Stock name")
+
+if (but):
+	st.write(title)
+	data = pd.read_json(httpx.get('http://172.17.0.1:8000/getStocks/' + title))
+	data1=pd.DataFrame(data)
+	table=st.table(data1)
+
+
 
