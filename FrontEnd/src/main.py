1cc6a9a40632e9dceccabed2d9290d70c109866b
Gal M <gal@LAPTOP-DC7TDUNP.localdomain>
2023-01-19 17:36:35 +0200

    added login form in frontend and integrated it with backend

--- FrontEnd/src/main.py
+++ FrontEnd/src/main.py
@@ -13,6 +13,15 @@ import httpx
 
 # Press the green button in the gutter to run the script.
 #if __name__ == '__main__':
-mainTitle=st.title ("""hello Gal""")
+st.session_state.username = ''
+def user_update(name):
+    st.session_state.username = name
+
+login_form = st.sidebar.form(key='signin_form', clear_on_submit=True)
+username = login_form.text_input(label='Enter Username')
+user_pas = login_form.text_input(label='Enter Password', type='password')
+if(httpx.post("http://172.17.0.1:8000/token", data = {'grant_type': '', 'username' : username, 'password' : user_pas, 'scope' : '' , 'client_id' : '', 'client_secret': ''}, headers = {'Content-Type': 'application/x-www-form-urlencoded', 'accept': 'application/json'})):
+    login = login_form.form_submit_button(label='Sign In', on_click = user_update(username))
+mainTitle=st.title ("""hello""" + st.session_state.username)
 st.sidebar.success("Select a demo above.")
 
