1e57416817b448342b0edfa944ed30989b94fd02
Gal M <gal@LAPTOP-DC7TDUNP.localdomain>
2023-01-20 15:36:03 +0200

    added a page that shows all of the stocks registered for the user in a graph

---
+++ FrontEnd/src/pages/showStocksListForUser.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import streamlit as st
+import json
+import pandas as pd
+import numpy as np
+import os
+import httpx
+
+#DATA_URL1 = "C:\Users\User\Desktop\BGU\gal\test"
+#DATA_URL = 'test'
+#columns=["name", "email"]
+
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+if (st.session_state.username):
+   mainTitle=st.title ("""hello """ + st.session_state.username)
+   js = httpx.get('http://172.17.0.1:8000/getStocksByUser?username=' + st.session_state.username).json()
+   df = pd.DataFrame.from_dict(js)
+   df.index = pd.to_datetime(df.index)
+   st.line_chart(df)
+
+
+
